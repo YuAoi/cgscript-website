30f5a3d7f715b53e5325aea2638a965b6bfa820a
YuAoi <zibo.z.liu@gmail.com>
2017-07-18 19:33:57 +0800

    initial rep

---
+++ src/components/Hello.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your CgScript PWA'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+h1 {
+  font-weight: normal;
+}
+</style>
