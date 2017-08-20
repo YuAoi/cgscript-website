20ed1cc7eadc088de20d785780e72f74383196f0
YuAoi <zibo.z.liu@gmail.com>
2017-08-21 00:41:25 +0800

    install dep

--- src/main.js
+++ src/main.js
@@ -2,11 +2,20 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import store from './store'
+
+import MuseUI from 'muse-ui'
+import 'muse-ui/dist/muse-ui.css'
+import '../static/material_icons/material-icons.css'
+
+Vue.use(MuseUI)
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   render: h => h(App)
 })
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,54 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import store from '@/store'
+// import Hello from '@/components/business/Hello'
+
+// import AdminLogin from '@/components/business/admin/Login'
 
 Vue.use(Router)
 
-export default new Router({
+const Hello = resolve => {
+  require.ensure(['@/components/business/Hello'], () => {
+    resolve(require('@/components/business/Hello'))
+  })
+}
+
+const AdminLogin = resolve => {
+  require.ensure(['@/components/business/admin/Login'], () => {
+    resolve(require('@/components/business/admin/Login'))
+  })
+}
+
+const router = new Router({
   routes: [
     {
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/admin',
+      name: 'Admin',
+      component: AdminLogin
     }
   ]
 })
+
+// 注册全局钩子拦截导航
+router.beforeEach((to, from, next) => {
+  let token = store.state.token
+  if (to.meta.requiresAuth) {
+    if (token) {
+      next()
+    } else {
+      next({
+        path: '/',
+        query: { redirect: to.fullPath }
+      })
+    }
+  } else {
+    next()
+  }
+})
+
+export default router
