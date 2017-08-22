ff98a3c5353d6dae17379db3ea92eaea98fd5c80
YuAoi <zibo.z.liu@gmail.com>
2017-08-22 21:21:51 +0800

    无静态文件列表

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,30 +1,27 @@
+// 因为webpack静态分析代码机制，暂无法实现运行时加载动态组件
+
 // import { deepEach } from '@/utils/deepMethods'
 
 // const ensure = function (path) {
-//   console.log(path)
-//   return resolve => {
-//     require.ensure([path], () => {
-//       resolve(require(path))
-//     })
-//   }
+//   return resolve => require([path], resolve)
 // }
+
+// const routes = [{
+//   path: '/',
+//   component: '@/components/business/Hello'
+// }, {
+//   path: '/admin',
+//   component: '@/components/business/admin/Login'
+// }]
+
+// deepEach(routes, route => (route.component = ensure(route.component)))
 
 const routes = [{
   path: '/',
-  component: resolve => {
-    require.ensure(['@/components/business/Hello'], () => {
-      resolve(require('@/components/business/Hello'))
-    })
-  }
+  component: resolve => require.ensure([], () => resolve(require('@/components/business/Hello')))
 }, {
   path: '/admin',
-  component: resolve => {
-    require.ensure(['@/components/business/admin/Login'], () => {
-      resolve(require('@/components/business/admin/Login'))
-    })
-  }
+  component: resolve => require.ensure([], () => resolve(require('@/components/business/admin/Login')))
 }]
-
-// deepEach(routes, route => (route.component = ensure(route.component)))
 
 export default routes
